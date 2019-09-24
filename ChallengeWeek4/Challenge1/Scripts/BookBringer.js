680b50f3eefc27897610dc91d2e7cfa244c2e62e
Jobsity <jobsity@mini.local>
2019-09-24 16:35:30 -0500

    Script advanced and trnaspilers working

--- ChallengeWeek4/Challenge1/Scripts/BookBringer.js
+++ ChallengeWeek4/Challenge1/Scripts/BookBringer.js
@@ -1,7 +1,86 @@
-const https = require("https");
 
-var bookISBN = {
-    "sda": "dasd"
+//IMPORTS
+let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+let fs = require('fs');
+
+//Base URL of the API
+const baseURL  = "https://www.googleapis.com/books/v1/volumes?q=isbn:"; //Hasta to have a ISBN book after
+const specificData = "https://www.googleapis.com/books/v1/volumes/"; // has to have and ID of a book after
+
+
+//Fifteen books to look for
+let books = [
+    '9781603090261',
+    '9781891830754',
+    '9781603090506',
+    '9781891830716',
+    '9781603090254',
+    '9781603090476',
+    '9781603093224',
+    '9781891830853',
+    '9781603090162',
+    '9781603092654',
+    '9781603092395',
+    '9781603090773',
+    '9781603093699',
+    '9781603090698',
+    '9781603090421'
+];
+
+let fieldsSpecifics = {
+    "general": [
+        'title',
+        'authors',
+        'publishedDate',
+        'averageRating',
+        'description',
+        'pageCount',
+        'imageLinks'
+    ],
+    "specifics": [
+        'categories',
+        ]
 };
+
+
+let getBook = (book) => {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", baseURL+books[0], false);
+    xhr.send(null);
+
+
+    if (xhr.status == 200) {
+        let object = JSON.parse(xhr.responseText);
+        console.log(object['items']);
+    }
+};
+
+let getBooksSpecific = (id) => {
     
-console.log(bookISBN["sda"]);
+}
+
+
+//Save final JSON inside file
+let writeToFile = (err, file) => {
+    if (err){
+        console.log("eddr");
+        return ;
+    }
+
+    console.log("DOne");
+    
+}
+
+let finalData = [];
+books.forEach(book => {
+    let result = getBook(book);
+    console.log(result);
+});
+
+
+//let file = fs.writeFile("./data.json", xhr.responseText, callToApi);
+
+//const https = require("https");
+
+
--- ChallengeWeek4/Challenge1/gulpfile.js
+++ ChallengeWeek4/Challenge1/gulpfile.js
@@ -12,29 +12,34 @@
 //     .pipe(babel())
 //     .pipe(gulp.dest("dist"));
 // });
-let gulp = require('gulp'),
-  sass = require('gulp-sass'),
-  uglify = require('gulp-uglify'),
-  pump = require('pump');
-gulp.task('default', function() {
-  console.log('Hello Gulp!');
+let gulp = require('gulp');
+const babel = require('gulp-babel');
+let sass = require('gulp-sass');
+let uglify = require('gulp-uglify');
+let  pump = require('pump');
+
+gulp.task('move:html', function () {
+  gulp.src('./src/*.html')
+    .pipe(gulp.dest('./dist'));
 });
 
-
-gulp.task('build:js', function(callback) {
-  pump([
-    gulp.src('src/**/*.js'),
-    uglify(),
-    gulp.dest('dist')
-  ], callback);
+gulp.task('build:es6', function() {
+  gulp.src('./src/es6/**/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('build:sass', function (callback) {
-  return gulp.src("./sass/**/*.scss")
+gulp.task('build:sass', function () {
+  return gulp.src("./src/scss/**/*.scss")
   .pipe(sass().on("error", (e) => {console.log(e);}))
-  .pipe(gulp.dest('./css'));
+  .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('watch:sass', function (callback) {
-  gulp.watch("./sass/**/*.scss", ['sass']);
-})
+
+gulp.task('watch', function () {
+  gulp.watch('src/*.html', gulp.parallel('move:html'))
+  gulp.watch('src/scss/**/*.scss', gulp.parallel('build:sass'));
+  gulp.watch('src/es6/**/*.js', gulp.parallel('build:es6'));
+});
