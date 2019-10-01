eea9da2dad187778e106477fc1e5573f77dc2284
Jobsity <jobsity@mini.local>
2019-10-01 15:38:02 -0500

    Mongoose set up and fisrt books schema ready

--- ChallengesWeek5/Challenge1/README.md
+++ ChallengesWeek5/Challenge1/README.md
@@ -1,5 +1,9 @@
+## Other important advancedments
+Advancing
+
 ## Setup the backend
-1. Install mongodb
+1. Clone or download the project and decompress it if required
+2. Npm install to donwload all dependencies
 
 ### Approaches
 - Reuse of BookResolver librerie done before
---
+++ ChallengesWeek5/Challenge1/book-shelf-backend/src/Schemas/books.schema.ts
@@ -0,0 +1,12 @@
+import * as mongoose from 'mongoose';
+
+export const bookSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    authors: [],
+    publishedDate: String,
+    pageCount: Number,
+    imageLinks: [],
+    city: String,
+    type: String,
+})
