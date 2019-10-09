32152da2a86571b7684c22a4c90ef46efaad5d13
David E Cermeno Pizon <daecepi@gmail.com>
2019-10-09 16:17:57 -0500

    Login update

--- ChallengesWeek6/Challenge1/Frontend/src/components/searchComponent/searchComponent.jsx
+++ ChallengesWeek6/Challenge1/Frontend/src/components/searchComponent/searchComponent.jsx
@@ -3,15 +3,24 @@ import React, { Component } from 'react';
 import './searchComponent.scss';
 
 class SearchComponent extends Component {
-    state = {  }
-    render() { 
+
+    constructor(props){
+        super(props);
+
+        this.state = {  };
+    }
+
+    render() {
+        let { type, placeholder, iconClasses } = this.props;
         return (
             <div className="inputWithIcon">
-                <input type="text" placeholder="Search.."></input>
-                <i className="fas fa-search" aria-hidden="true"></i>
+                <input type={type} placeholder={placeholder}></input>
+                {iconClasses ? <i className={iconClasses} aria-hidden="true"></i> : ""}
             </div>
         );
     }
 }
- 
+
+
+//{iconClasses ? <i className={iconClasses} aria-hidden="true"></i> : ""}
 export default SearchComponent;
