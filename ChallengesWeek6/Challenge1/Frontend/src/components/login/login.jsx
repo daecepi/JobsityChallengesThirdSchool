54185b765bb5feaec4d08bd703ca567a8c900479
Jobsity <jobsity>
2019-10-10 10:34:06 -0500

    Login method assotiation with login button and span tag added pt1

--- ChallengesWeek6/Challenge1/Frontend/src/components/login/login.jsx
+++ ChallengesWeek6/Challenge1/Frontend/src/components/login/login.jsx
@@ -5,19 +5,30 @@ import './login.scss';
 import SearchComponent from '../searchComponent/searchComponent';
 
 class Login extends Component {
-    state = {  }
-    render() { 
+    state = { 
+        username: undefined,
+        password: undefined,
+        message: "asdlas"
+     }
+
+    authenticate = async ( )=> {
+        alert('You shouldnt0');
+        let authResult = await fetch('localhost:5000/login',);
+    }
+
+    render() {
         return (
             <div className="full-container">
                 <div className="container">
                     <h1 className="login-title">Jobsity Login</h1>
                     <div className="input">
-                        <SearchComponent type="password" placeholder="Username..." iconClasses="fas fa-user-circle"/>
+                        <SearchComponent id="" type="text" placeholder="Username..." iconClasses="fas fa-user-circle"/>
                     </div>
                     <div className="input">
                         <SearchComponent type="password" placeholder="Username..." iconClasses="fas fa-lock"/>
                     </div>
-                    <button className="button" submit="submit" value="Login">Login</button>
+                    <button onClick={this.authenticate} className="button" value="Login">Login</button>
+                    {this.state.message ? <span className="messages">{this.state.message}</span> : ""}
                 </div>
             </div>
          );
