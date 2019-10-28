c400ffbf56735651d1c57c866fa93a424ca88167
Jobsity <jobsity>
2019-10-28 15:55:08 -0500

    Color name improvement

--- ChallengesWeek8/Challenge1/frontend/src/styles/colors.js
+++ ChallengesWeek8/Challenge1/frontend/src/styles/colors.js
@@ -1,24 +1,24 @@
 //General layout
 //General layout
-export const primary_white = {color: "#fafcfe", alpha: 1, rgb: "rgba(250, 252, 254, 1);"};
-export const secondary_white= {color: "#f7f3eb", alpha: 1, rgb: "rgb(247, 243, 235);"};
-export const third_white= {color: "#f0f0f0", alpha: 1, rgb: "rgb(240, 240, 240);"};
+export const primaryWhite = {color: "#fafcfe", alpha: 1, rgb: "rgba(250, 252, 254, 1);"};
+export const secondaryWhite= {color: "#f7f3eb", alpha: 1, rgb: "rgb(247, 243, 235);"};
+export const thirdWhite= {color: "#f0f0f0", alpha: 1, rgb: "rgb(240, 240, 240);"};
 
-export const primary_blue = {color: "#60b5d6", alpha: 1, rgb: "rgb(96, 181, 214);"};
-export const primary_blue_transparent= {color: "#60b5d6", alpha: 0.5, rgb: "rgba(96, 181, 214, 0.5);"};
-export const letters_blue_darker = {color: "·2f97f2", alpha: 1, rgb: "rgba(47, 151, 242, 1);"};
-export const secondary_blue = {color: "#2f97f2", alpha: 1, rgb: "rgba(47, 151, 242, 1);"};
+export const primaryBlue = {color: "#60b5d6", alpha: 1, rgb: "rgb(96, 181, 214);"};
+export const primaryBlueTransparent= {color: "#60b5d6", alpha: 0.5, rgb: "rgba(96, 181, 214, 0.5);"};
+export const lettersBlueDarker = {color: "·2f97f2", alpha: 1, rgb: "rgba(47, 151, 242, 1);"};
+export const secondaryBlue = {color: "#2f97f2", alpha: 1, rgb: "rgba(47, 151, 242, 1);"};
 
-export const primary_dark = {color: "#041d29", alpha: 1, rgb: "rgba(4, 29, 41, 1);"};
-export const primary_dark_transparent = { color: "#041d29", alpha: 0.5, rgb: "rgba(4, 29, 41, 0.5);"};
-export const secondary_dark = { color: "#3f3f3f", alpha: 0.9, rgb: "rgba(63, 63, 63, 0.9);"};
-export const third_dark = { color: "#2e4052", alpha: 1, rgb: "rgb(46, 64, 82);"};
+export const primaryDark = {color: "#041d29", alpha: 1, rgb: "rgba(4, 29, 41, 1);"};
+export const primaryDarkTransparent = { color: "#041d29", alpha: 0.5, rgb: "rgba(4, 29, 41, 0.5);"};
+export const secondaryDark = { color: "#3f3f3f", alpha: 0.9, rgb: "rgba(63, 63, 63, 0.9);"};
+export const thirdDark = { color: "#2e4052", alpha: 1, rgb: "rgb(46, 64, 82);"};
 
-export const primary_grey = { color: "#a09e99", alpha: 1, rgb: "rgb(160, 158, 153);"};
-export const secondary_grey = { color: "#6e6d69", alpha: 1, rgb: "rgb(110, 109, 105);"};
+export const primaryGrey = { color: "#a09e99", alpha: 1, rgb: "rgb(160, 158, 153);"};
+export const secondaryGrey = { color: "#6e6d69", alpha: 1, rgb: "rgb(110, 109, 105);"};
 
-export const line_gradient= "linear_gradient(white 0%,white 10%,black 10%, black 80%,white 80%);";
+export const lineGradient= "linear_gradient(white 0%,white 10%,black 10%, black 80%,white 80%);";
 
 // Color for state mesages
-export const primary_error = { color: "#ff0000", alpha: 1, rgb: "red:" };
-export const primary_success = { color: "#00ff00", alpha: 1, rgb: "green;" };
+export const primaryError = { color: "#ff0000", alpha: 1, rgb: "red:" };
+export const primarySuccess = { color: "#00ff00", alpha: 1, rgb: "green;" };
