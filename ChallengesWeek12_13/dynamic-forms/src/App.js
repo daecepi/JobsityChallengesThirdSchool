0ff251a2a9846f5c3f1656e8412076cdf6d281ac
Jobsity <jobsity>
2019-11-28 18:21:10 -0500

    Baseform added

--- ChallengesWeek12_13/dynamic-forms/src/App.js
+++ ChallengesWeek12_13/dynamic-forms/src/App.js
@@ -2,24 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import BaseForm from './components/BaseForm';
+
 function App() {
+  const baseObject = {
+    a: 23,
+    b: 33,
+  };
+
+  const other = ;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BaseForm />
   );
 }
 
