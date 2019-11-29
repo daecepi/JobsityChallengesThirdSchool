f5256a3c7fdfbe7855c72be6a703cc1c07415db6
Jobsity <jobsity>
2019-11-29 18:10:21 -0500

    Service for reading forms advanced

--- ChallengesWeek12_13/dynamic-forms/src/components/BaseForm/index.js
+++ ChallengesWeek12_13/dynamic-forms/src/components/BaseForm/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { formStructure } from '../../defaults';
 
 import { Tabs, Tab } from "react-bootstrap";
 
@@ -6,18 +7,44 @@ import Card from "../Card";
 
 import { BaseFormWrapper } from "../../styles/baseComponents";
 
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 function BaseForm() {
-  const [key, keyChange] = useState("home");
+  
+  function loadForm () {
+    const formGotten = localStorage.getItem('form');
+    if(!formGotten){
+        localStorage.setItem('form', JSON.stringify(formStructure));
+        return formStructure;      
+    }
+    return formGotten;
+  }
 
+  const [key, keyChange] = useState("DeviceInfo");
+  const [formData, changeFormData] = useState(loadForm); 
 
-  loa
+  console.log("Form data acquired", formData);
 
   return (
     <Card title="Base component">
       <BaseFormWrapper>
         <Tabs did="controlled-tab-example" activeKe y={key} onSelect={k => keyChange(k)}>
+          {
+              formData.tabs.map( tabs =>{
+                return (
+                    <Tab eventKey={tabs.id} title={tabs.name}>
+                        {
+                            tabs.attributes.map( attribute => {
+
+                            })
+                        }
+                    </Tab>
+                );
+              })
+          }
+          
           <Tab eventKey="DeviceInfo" title="Home">
             <p>HOla</p>
           </Tab>
--- ChallengesWeek12_13/dynamic-forms/src/services/index.js
+++ ChallengesWeek12_13/dynamic-forms/src/services/index.js
@@ -1,3 +1,6 @@
+/**
+ * Function that will be used to fetch the saved form structure or bring the default one
+ */
 export const fetchData = async () => {
     let data = await fetch('');
 };
